af77019267f83e24bdd939d10d1ab3d3dbfb2a63
Femi Laizer <90426320+femiscript@users.noreply.github.com>
2022-02-13 14:42:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+
+let btn = document.querySelector('.btn');
+
+btn.onclick = function () {
+    document.querySelector("body").classList.add("blur");
+    document.querySelector(".project").classList.add("show");
+}
